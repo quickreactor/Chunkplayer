e901c84a9b69901a9ddbbc86ffe614f00dc42b8f
Barnaby Fredric <36432141+quickreactor@users.noreply.github.com>
2025-01-23 00:23:15 +1300

    update robmorb

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ videoPlayer.addEventListener("loadedmetadata", () => {
 const selector = document.getElementById("chunkSelector");
 const numberDisplay = document.querySelector(".numberDisplay");
 let morbCount = 0;
-let robMorbCount = 13;
+let robMorbCount = 14;
 let randomNumber = 0;
 // uncomment to get around sunday detection
 // today = new Date('2024-10-30');
